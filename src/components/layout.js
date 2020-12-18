0aed7ffb63dc8410fa3ba8db72842e3c4816fda8
Conor Mendenhall <conor.mendenhall@gmail.com>
2020-12-18 17:00:49 -0500

    replace materialize with bulma

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,24 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import 'materialize-css'
-import { Navbar, NavItem, Icon } from 'react-materialize'
-import '../styles/materialize.scss'
 
 export default function Layout({ children }) {
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>Good Box</title>
-        <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-        />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Good Box</title>
       </Helmet>
       <main>
-        <Navbar
-          alignLinks="right"
-          brand={
-            <a className="brand-logo" href="#">
-              Good Box
-            </a>
-          }
-          id="mobile-nav"
-          menuIcon={<Icon>menu</Icon>}
-          options={{
-            draggable: true,
-            edge: 'left',
-            inDuration: 250,
-            onCloseEnd: null,
-            onCloseStart: null,
-            onOpenEnd: null,
-            onOpenStart: null,
-            outDuration: 200,
-            preventScrolling: true,
-          }}
-        >
-          <NavItem href="#sendABox">Send A Box</NavItem>
-          <NavItem href="#partners">Partners</NavItem>
-        </Navbar>
+        <nav>
+          <a className="brand-logo" href="#">
+            Good Box
+          </a>
+          <a href="#sendABox">Send A Box</a>
+          <a href="#partners">Partners</a>
+        </nav>
         {children}
       </main>
-      <script
-        type="text/javascript"
-        src="utils/bin/materialize.min.js"
-      ></script>
     </>
   )
 }
